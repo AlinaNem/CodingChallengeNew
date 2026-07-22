7831da4768746aeffa18294adc3d60eb8d404134
Alina <nematjanova15@mail.ru>
2026-07-23 00:27:01 +0600

    Revert "This commit contains bug"
    This reverts commit 9ab31d5c8539ff24f21b4cb01db1eceeeb6fe9ff.

--- src/main/java/org/spring/revert/RevertDemo.java
+++ src/main/java/org/spring/revert/RevertDemo.java
@@ -4,6 +4,5 @@ public class RevertDemo {
     public static void main(String[] args) {
 
         System.out.println("This line has a bug!!!");
-        System.out.println("This bug has been fixed!!!");
     }
 }
